ed6bef690ad3c498c7185876c081037b8f5ab183
P.Sannith <88277591+Sannith-Hack@users.noreply.github.com>
2025-09-20 09:16:56 +0530

    Update dbUtils.js to take user id

--- client/server/lib/dbUtils.js
+++ client/server/lib/dbUtils.js
@@ -3,7 +3,7 @@ import pool from './db.js'; // Import the shared pool
 
 const userTableQuery = `CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
+    userid VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     phone VARCHAR(15) UNIQUE,
